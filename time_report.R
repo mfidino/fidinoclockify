37c512f47678b16705cff6162de83cb30848b6e4
Mason Fidino <mfidino@lpzoo.org>
2019-03-08 11:49:55 -0600

    initial commit

---
+++ time_report.R
@@ -0,0 +1,56 @@
+
+library(httr)
+
+  clock <- httr::VERB(
+    verb = "GET",
+    url = paste0("https://api.clockify.me/api/workspaces/",
+                 .api$clockify$workspaceID,"/timeEntries/"),
+    httr::add_headers(`X-Api-Key` = .api$clockify$api),
+    encode = "json"
+  )
+
+  GET /workspaces/{workspaceId}/clients
+  hm <- httr::VERB(
+    verb = "GET",
+    url = paste0("https://api.clockify.me/api/workspaces/",.api$clockify$workspaceID,
+                 "/clients"),
+    httr::add_headers(`X-Api-Key` = .api$clockify$api),
+    encode = "json"
+  )
+
+
+
+  #/workspaces/{workspaceId}/timeEntries/
+
+
+
+    times <- httr::VERB(
+      verb = "GET",
+      url = paste0("https://api.clockify.me/api/workspaces/",.api$clockify$workspaceID,
+                   "/timeEntries/"),
+      httr::add_headers(`X-Api-Key` = .api$clockify$api)#,
+      #encode = "json"
+    )
+
+    # need to make a function to pull the details
+
+
+
+      POST(url = paste0("https://api.clockify.me/api/workspaces/",.api$clockify$workspaceID,
+                   "/reports/summary/"),
+      httr::add_headers(`X-Api-Key` = .api$clockify$api,
+                        `Content-Type`="application/json"),
+      encode = "json",
+      body = list(startDate = "2019-03-04T00:00:00.000Z",
+                  endDate = "2019-03-08T23:59:59.999Z",
+                  me = "false",
+                  includeTimeEntries = "true",
+                  billable = "both",
+                  zoomLevel = "week",
+                  description = "",
+                  archived = "Active",
+                  roundingOn = "false"),
+      httr::verbose()
+    )
+
+
